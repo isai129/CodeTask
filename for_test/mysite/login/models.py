2756f868ea386beaf49779089532778f7af65368
initial <isai129@live.com>
2024-09-29 17:37:33 +0800

    fix

--- for_test/mysite/login/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-
-class UserInfo(models.Model):  # 要继承这个类（固定写法）。
-    user = models.CharField(max_length=32)
-    pwd = models.CharField(max_length=32)
-    # 创建两个字段，最大长度32，类型是char
